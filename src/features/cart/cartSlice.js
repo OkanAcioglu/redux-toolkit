0266e7b4b11b3a10209c01242c671cbe0180cd48
Okan <okanacioglu@gmail.com>
2022-12-18 23:24:33 +0300

    increase-decrease

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -32,6 +32,15 @@ const cartSlice = createSlice({
       const itemId = action.payload
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
     },
+    increase: (state, { payload }) => {
+      //! In this case payload is an object so we go with payload.id
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
+    },
   },
 })
 
@@ -39,5 +48,5 @@ const cartSlice = createSlice({
 //! That is why we wanna export it...
 console.log(cartSlice)
 
-export const { clearCart, removeItem } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions
 export default cartSlice.reducer
